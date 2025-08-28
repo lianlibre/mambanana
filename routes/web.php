a6ca18d1547a2ac9cbdfa63ebfb266c5fa3958c6
lianlibre <lianlibre491@gmail.com>
2025-08-28 12:57:44 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/clear-cache', function() {
+    Artisan::call('optimize:clear');
+    return 'Cache cleared';
+});
+
